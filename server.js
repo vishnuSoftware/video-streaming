c54f930628db04c4ae24343117db342f3d154e79
sundar <sundar@ardhika.com>
2016-07-08 13:22:06 +0530

    testing2

--- server.js
+++ server.js
@@ -31,4 +31,5 @@ require("./server/routes.js")(app);
 
 app.listen(port);
 console.log('App is listening on port: ' + port);
-console.log('Everything fine!  by vishnu' );
+console.log('Everything fine!' );
+console.log('Everything fine ! By bala' );
